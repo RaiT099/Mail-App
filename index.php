e6eee66b0d0c24f3abf5102887016d85bf4c39ca
RaiT099 <88438811+RaiT099@users.noreply.github.com>
2022-02-23 18:46:18 +0800

    Add files via upload
    https://www.youtube.com/watch?v=HBDJsc2cXR4

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+
+$mail->Username = 'raitochong@gmail.com';
+$mail->Password = 'Raito@5791';
+
+$mail->setFrom('raitochong@gmail.com', 'Raito');
+$mail->addAddress('kwongwei135@gmail.com');
+$mail->addReplyTo('raitochong@gmail.com');
+
+$mail->isHTML(true);
+$mail->Subject = 'PHP Mailer Subject';
+$mail->Body = '<h1 align = center>Test</h>';
+$mail->addAttachment('C:\Users\ASUS\Pictures\Anime\kv_ubw.png');
+
+if(!$mail->send()){
+    echo 'Message could not be sent';
+} else {
+    echo 'Message has been sent!';
+}
+
+?>
